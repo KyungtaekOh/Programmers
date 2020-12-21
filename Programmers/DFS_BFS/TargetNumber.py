501e2b4dc52d871b53de00716cbff086367d47a8
Taek <rudxor5026@gmail.com>
2020-12-21 20:11:33 +0900

    Target Number

---
+++ Programmers/DFS_BFS/TargetNumber.py
@@ -0,0 +1,31 @@
+def solution(numbers, target):
+    length = len(numbers)
+    answer = dfs(numbers, target, 0, 0, length)
+
+    return answer
+
+
+def dfs(nums, tar, val, cnt, length):
+    if (length == cnt):
+        if (val == tar):
+            return 1
+        else:
+            return 0
+
+    left = dfs(nums, tar, val + nums[cnt], cnt + 1, length)
+    right = dfs(nums, tar, val - nums[cnt], cnt + 1, length)
+
+    return left + right
+
+
+if __name__ == '__main__':
+    numbers = [1,1,1,1,1]
+    target = 3
+    answer = 5
+
+    sol = solution(numbers, target)
+
+    """
+    First solve : 2020-12-21
+
+    """
