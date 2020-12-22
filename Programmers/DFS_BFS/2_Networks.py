29b930fde71b7806550c66cc7f40f5540b3a0cc6
Taek <rudxor5026@gmail.com>
2020-12-22 14:18:17 +0900

    Solved using BFS in queue form

--- Programmers/DFS_BFS/2_Networks.py
+++ Programmers/DFS_BFS/2_Networks.py
@@ -1,18 +1,21 @@
+"""
+First solve : 2020-12-22
+"""
+
 def solution(n, coms):
     answer = 0
-    for a in range(n):  # n번째 행으로 새로운 네트워크를 시작
+    for a in range(n):                      # n번째 행으로 새로운 네트워크를 시작
         if (coms[a][a] != -1):
             answer += 1
             net = [a]
-            while (len(net) != 0):  # n개의 행을 반복
+            while (len(net) != 0):          # n개의 행을 반복
                 line = net.pop(0)
                 if (coms[line][line] == 1):
                     coms[line][line] = -1
-                    for i in range(n):  # n개의 열을 반복하여 넷웤 찾기
+                    for i in range(n):      # n개의 열을 반복하여 넷웤 찾기
                         if (coms[line][i] == 1):
                             net.append(i)
-        else:
-            continue
+        else:continue
     return answer
 
 
