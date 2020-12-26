fce2d1e4b3c72a58212d7ea68f8a2e2399c0b5dd
Taek <rudxor5026@gmail.com>
2020-12-26 17:24:28 +0900

    Add solution about other way to solve

--- Programmers/DFS_BFS/1_TargetNumber.py
+++ Programmers/DFS_BFS/1_TargetNumber.py
@@ -1,6 +1,7 @@
 """
 First solve : 2020-12-21
     - target을 줄이는 방식으로해도 가능할것 같다.
+
 """
 
 def solution(numbers, target):
@@ -22,12 +23,22 @@ def dfs(nums, tar, val, cnt, length):
 
     return left + right
 
+### other version
+def solution2(numbers, target):
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target - numbers[0]) + solution(numbers[1:], target + numbers[0])
+
 
 if __name__ == '__main__':
     numbers = [1, 1, 1, 1, 1]
     target = 3
     answer = 5
 
-    sol = solution(numbers, target)
+    sol = solution2(numbers, target)
+    print(sol)
 
 
