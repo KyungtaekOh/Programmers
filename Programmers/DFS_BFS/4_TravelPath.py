30a93313b78d8c45350da85cea639022dd87431e
Taek <rudxor5026@gmail.com>
2020-12-28 18:53:23 +0900

    Solved it yet. Just idea scatch

---
+++ Programmers/DFS_BFS/4_TravelPath.py
@@ -0,0 +1,16 @@
+def solution(tickets):
+    """
+    1. 출발지 선택
+    2. 사이클 검색
+    3. 사이클의 순서 결정
+    """
+    answer = []
+    return answer
+
+
+if __name__ == '__main__':
+    tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+
+    result = solution(tickets)
+    print(result)
+    # Expect : 	["ICN", "JFK", "HND", "IAD"]
