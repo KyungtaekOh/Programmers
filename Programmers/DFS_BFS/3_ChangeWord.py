4cc894e30120c64c4999089d5d8e6fa8d4b0b8e5
Taek <rudxor5026@gmail.com>
2020-12-28 15:58:28 +0900

    Solved using BFS in queue form and dictionary
    Frist solve
        try 1 : DFS 에서 too many recursive depth
        try 2 : BFS 에서 cycle이 생성될 수 있음을 인지
        try 3 : mydict의 형태가 결국 tree가 아닌 graph임을 인지
                -> 출발점이 있는 graph이므로 BFS를 통해 level단위 검색
                -> graph의 형태이므로 visited를 통해 cycle 방지

--- Programmers/DFS_BFS/3_ChangeWord.py
+++ Programmers/DFS_BFS/3_ChangeWord.py
@@ -1,5 +1,8 @@
-from collections import defaultdict
+"""
+First solve : 2020-12-28
+"""
 
+from collections import defaultdict
 
 def solution(begin, target, words):
     if not (target in words): return 0  # words에 없을 경우
@@ -26,56 +29,37 @@ def solution(begin, target, words):
             if (diff == 1):
                 mydict[find].add(words[j])
 
-    def dfs2(word):
-        visited = []
-        stack = [word]
-        while(stack):
-            find = stack.pop()
-            line = list(mydict[find])
-            # while(line):
-
-
-
-    ### BFS ###
-    """
-    2 try : while문을 돌며 stack에 쌓을 때 루프가 발생할 수 있음
-    """
     def bfs(word):
-        # stack
-        # while에서 loop 발생 가능
-        stack = set()
-        depth = 0
-        stack.add(depth)
-        stack.add(word)
-        while(stack):
-            find = stack.pop()
-            dic = list(mydict[find[1]])
-            if target in dic:
-                #여기서 반복문돌려서 최근숫자
-                return
-            for i in dic:
-                stack.add([find[0] + 1, i])
+        visited = set()
+        queue = [word]
+        level = 0
+        while(True):
+            q_len = len(queue)
+            for j in range(q_len):
+                find = queue.pop(0)
+                visited.add(find)
+                # 각 레벨의 단어를 찾는다
+                line = list(mydict[find])
+                for i in line:
+                    if not (i in visited):
+                        queue.append(i)
+                    if(i == target):
+                        return level + 1
+            level += 1
 
-    ### DFS ###
-    """
-    1 try : too many recursive depth
-    이걸 중복되게 안하면 될거같은데..
-    """
-    def dfs(word):
-        dic = list(mydict[word])
-        if target in dic: return 0
-        depth = 9999
-        for i in dic:
-            value = dfs(i)
-            depth = min(depth, value)
-        return depth + 1
-
-    return dfs(begin)
-
+    return bfs(begin)
+"""
+Frist solve
+    try 1 : DFS 에서 too many recursive depth
+    try 2 : BFS 에서 cycle이 생성될 수 있음을 인지
+    try 3 : mydict의 형태가 결국 tree가 아닌 graph임을 인지
+            -> 출발점이 있는 graph이므로 BFS를 통해 level단위 검색
+            -> graph의 형태이므로 visited를 통해 cycle 방지
+"""
 
 if __name__ == '__main__':
     begin = "hit"
     target = "cog"
-    words = ["hot", "dot", "dog", "lot", "log", "cog"]
-    result = solution(begin, target,words)
+    words = ["hot", "hor", "tor", "tog", "cog"]
+    result = solution(begin, target, words)
     print(result)
