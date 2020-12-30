b1fa08950d8438d3fd2569ac3b2a077951f0b45f
Taek <rudxor5026@gmail.com>
2020-12-30 16:35:47 +0900

    solved it yet

---
+++ Programmers/Graph/1_FarthestNode.py
@@ -0,0 +1,18 @@
+def solution(n, edge):
+    """
+    DFS in Graph?
+    """
+
+
+    answer = 0
+    return answer
+
+
+
+if __name__ == '__main__':
+    n = 6
+    vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+
+    result = solution(n, vertex)
+    print(result)
+    # Expect : 	3
