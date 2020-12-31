5ccda24ef9624a9387d36e089fd60e5fab701180
Taek <rudxor5026@gmail.com>
2020-12-31 14:59:55 +0900

    Solved it yet

--- Programmers/DFS_BFS/4_TravelPath.py
+++ Programmers/DFS_BFS/4_TravelPath.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def solution(tickets):
     """
     1. 출발지 선택
@@ -5,12 +8,25 @@ def solution(tickets):
     3. 사이클의 순서 결정
     """
     answer = []
+    mydict = defaultdict(set)
+    ### Make Dictionary
+    for i, j in tickets:
+        mydict[i].add(j)
+
+    def dfs(start, dict=mydict):
+
+        return 0
+
+    for i in mydict:
+        for j in mydict[i]:
+            answer = dfs(j)
+
     return answer
 
 
 if __name__ == '__main__':
-    tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+    tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
 
     result = solution(tickets)
     print(result)
-    # Expect : 	["ICN", "JFK", "HND", "IAD"]
+    # Expect : ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
