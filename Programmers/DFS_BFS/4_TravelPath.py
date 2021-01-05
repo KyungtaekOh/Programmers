459e88ba06384bd5fb53de9196586af9cbc260b1
Taek <rudxor5026@gmail.com>
2021-01-05 14:00:01 +0900

    Solved using DFS except testcase 1...

--- Programmers/DFS_BFS/4_TravelPath.py
+++ Programmers/DFS_BFS/4_TravelPath.py
@@ -1,32 +1,37 @@
 from collections import defaultdict
-
+from copy import deepcopy
 
 def solution(tickets):
-    """
-    1. 출발지 선택
-    2. 사이클 검색
-    3. 사이클의 순서 결정
-    """
-    answer = []
-    mydict = defaultdict(set)
+    length = len(tickets) + 1
+    answer = [0] * length
+    sticket = sorted(tickets, key=lambda k: (k[0], k[1]))
+
+    mydict = defaultdict(list)
     ### Make Dictionary
-    for i, j in tickets:
-        mydict[i].add(j)
+    for i, j in sticket:
+        mydict[i].append(j)
+    print('hello', mydict)
 
-    def dfs(start, dict=mydict):
+    ### DFS
+    def dfs(node, mdict, depth, target, ans):
+        if(depth == target) :
+            ans[-1] = node
+            return ans
+        line = mdict[node]
+        ans[depth-1] = node
+        if(len(line) == 0) : return False
+        for i in line:
+            temp = deepcopy(mdict)
+            temp[node].remove(i)
+            res = dfs(i, temp, depth+1, target, ans)
+            if not (res == False) : return ans
 
-        return 0
-
-    for i in mydict:
-        for j in mydict[i]:
-            answer = dfs(j)
-
-    return answer
-
+    return dfs('ICN', mydict, 1, length, answer)
 
 if __name__ == '__main__':
-    tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
+    n = [['ICN', 'A'], ['A', 'C'], ['A', 'D'], ['D', 'B'], ['B', 'A']]
+    # tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "SFO"], ["ATL", "ICN"]]
+    # tickets = 	[["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 
-    result = solution(tickets)
+    result = solution(n)
     print(result)
-    # Expect : ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
