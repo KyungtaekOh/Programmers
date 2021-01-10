c2a66545c53791fd2c2194d6e818b8ad449dae69
Taek <rudxor5026@gmail.com>
2021-01-10 20:12:35 +0900

    Solving using recursive DFS
    I think it would be better not to use a recursive.

--- Programmers/Graph/2_Ranking.py
+++ Programmers/Graph/2_Ranking.py
@@ -1,3 +1,6 @@
+"""
+First solve : 2021-01-10
+"""
 def solution(n, results):
     answer = 0
     score = [[0]*n for i in range(n)]
@@ -9,15 +12,18 @@ def solution(n, results):
         score[l-1][w-1] = 2
         win[w].append(l)
         lose[l].append(w)
-    clear(n, score)
 
-    def bfs(now, find=1):
+    def bfs(now, find=1, visited=set()):
         w = []
+
         if (find == 1):temp = win[now]
         else: temp = lose[now]
 
+        if (now in visited): return temp
+        else: visited.add(now)
+
         for i in temp:
-            w += bfs(i, find)
+            w += bfs(i, find, visited)
         w += temp
         w = list(set(w))
         for i in w:
@@ -25,24 +31,19 @@ def solution(n, results):
         return w
 
     for i in range(1, n+1):
-        bfs(i, 1)
-        bfs(i, 2)
+        nset = set()
+        bfs(i, 1, nset)
+        nset.clear()
+        bfs(i, 2, nset)
         if(score[i-1].count(0) == 1):
             answer += 1
-    clear(n, score)
     return answer
 
-def clear(n, temp):
-    print("\  ", end='')
-    for i in range(n):
-        print(str(i)+"  ", end='')
-    print()
-    for i,line in enumerate(temp):
-        print(i, line)
-    print()
-
 if __name__ == '__main__':
-    n = 7
-    results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5], [2, 6], [6, 5], [5, 7]]
+    # n = 7
+    # results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5], [2, 6], [6, 5], [5, 7]]
+
+    n = 5
+    results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
     sol = solution(n, results)
     print("answer:", sol)
