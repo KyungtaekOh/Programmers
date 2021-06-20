117a2d00ca0dbe920718517a68152f79b240f1ab
Taek <rudxor5026@gmail.com>
2021-06-20 17:06:46 +0900

    2020 KAKAO BLIND RECRUITMENT

---
+++ Programmers/Others/[CodingTest]_Convert_Parentheses.py
@@ -0,0 +1,29 @@
+def divisor(p):
+    total = 0
+    for idx, c in enumerate(p):
+        if c == "(":
+            total += 1
+        elif c == ")":
+            total -= 1
+        if idx != 0 and total == 0:
+            return p[:idx + 1], p[idx + 1:]
+
+def solution(p):
+    answer = ''
+    if len(p) == 0:
+        return ''
+    u, v = divisor(p)
+    if u[0] == ")":
+        temp = "(" + solution(v) + ")" + ''.join([")" if i == "(" else "(" for i in u[1:-1]])
+        return temp
+    elif u[0] == "(":
+        answer += u + solution(v)
+    return answer
+
+"""
+Test case
+P           result
+"(()())()"	"(()())()"
+")("	    "()"
+"()))((()"	"()(())()"
+"""
