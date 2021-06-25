8532cdaec84f2e1711962cf19a8b77e504a79d03
Taek <rudxor5026@gmail.com>
2021-06-25 19:12:48 +0900

    Solve using BFS

--- Programmers/Graph/1_FarthestNode.py
+++ Programmers/Graph/1_FarthestNode.py
@@ -4,13 +4,13 @@ First solve : 2021-01-08
 def solution(n, edge):
     answer = 0
     mydict = [[] for i in range(n)]
-    for i, j in edge:
+    for i, j in edge:              # 경로 저장
         mydict[i - 1].append(j)
         mydict[j - 1].append(i)
+
     queue = [1]
     visited = [1] + [0 for i in range(n - 1)]
-
-    while (queue):
+    while (queue):                  # BFS
         answer = len(queue)
         for i in range(answer):
             node = queue.pop(0)
