56aa25eaae40dfe6b7ae1f074430a1c67aea5204
Taek <rudxor5026@gmail.com>
2021-08-02 20:04:26 +0900

    weekly challenge 01

---
+++ Programmers/Weekly_Challenge/week1.py
@@ -0,0 +1,11 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(count):
+        total += price*(i+1)
+    return total-money if total>money else 0
+
+"""
+Test case
+price	money	count	result
+3	    20	    4	    10
+"""
