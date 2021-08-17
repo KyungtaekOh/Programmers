f1290e6ede2691d77aadad0c44c8df969dcdc9d8
Taek <rudxor5026@gmail.com>
2021-08-18 00:20:02 +0900

    Weekly Challenge 03

--- Programmers/Weekly_Challenge/week03.py
+++ Programmers/Weekly_Challenge/week03.py
@@ -2,16 +2,14 @@ from collections import defaultdict, deque
 def getShape(board, target, row, col):
     visit, blen = set([(row, col)]), len(board)
     queue = deque([(row,col)])
-    shape = [(row,col)]
     while queue:                        # BFS 탐색
         y, x = queue.popleft()
         for r, c in [(0,1),(0,-1),(1,0),(-1,0)]:
             nr, nc = y+r, x+c
             if 0<=nr<blen and 0<=nc<blen and (nr,nc) not in visit and board[nr][nc] == target:
                 queue.append((nr, nc))
-                shape.append((nr, nc))
                 visit.add((nr,nc))
-    return shape, visit
+    return list(visit)
 
 def move_zero(block):                   # (0, 0)에 가장 가깝게 이동
     values = list(map(list, zip(*block)))
@@ -27,85 +25,61 @@ def rotate(block):                  # 4번 회전시켜서 min값을 반환
     for row, col in block:          # grid 형태로 변환
         grid_block[row][col] = 1
 
-    candi = []
-    for step in range(4):           # 4번 반복
+    candi = [block]
+    for step in range(3):           # 4번 반복
         temp = [[0]*size for _ in range(size)]
         for r in range(size):       # 회전
             for c in range(size):
                 temp[c][size-1-r] = grid_block[r][c]
-
         for r in range(size):       # 좌표단위로 변환
             flag = 0
             for c in range(size):
                 if temp[r][c] == 1:
-                    shape, visit = getShape(temp, 1, r, c)
+                    visit = getShape(temp, 1, r, c)
                     flag = 1
                     break
-            if flag == 1:
-                break
-        shape = move_zero(shape)
+            if flag == 1:break
+        shape = move_zero(visit)
         candi.append(shape)
         grid_block = temp
     return min(candi)
 
-# def rotate(block):
-#     if len(block) == 1: return block
-#     candi = []
-#     v = list(map(list, zip(*block)))
-#     h, w = max(v[0])-min(v[0]), max(v[1])-min(v[1])
-#
-#     for not_use in range(4):
-#         temp = []
-#         for pos in block:
-#             temp.append((pos[1], pos[0]))
-#         temp = [(r, w-c) for r, c in temp]
-#         temp = move_zero(temp)
-#         shape = temp
-#         candi.append(shape)
-#         h, w = w, h
-#     return min(candi)
-
 def find_location(board, table):
     blen = len(board)
-    bdict, bvisit = defaultdict(list), set()
+    location, bvisit = [], set()
     tdict, tvisit = defaultdict(list), set()
     for row in range(blen):
         for col in range(blen):
             if board[row][col] == 0 and (row,col) not in bvisit:
-                bshape, bvst = getShape(board, 0, row, col)
-                bshape = move_zero(bshape)
-                bshape = rotate(bshape)
-                bdict[len(bshape)].append(bshape)
-                for v in bvst:
+                bshape = getShape(board, 0, row, col)
+                for v in bshape:
                     bvisit.add(v)
+                bshape = rotate(bshape)
+                location.append(bshape)
             if table[row][col] == 1 and (row,col) not in tvisit:
-                tshape, tvst = getShape(table, 1, row, col)
-                tshape = move_zero(tshape)
+                tshape = getShape(table, 1, row, col)
+                for v in tshape:
+                    tvisit.add(v)
                 tshape = rotate(tshape)
                 tdict[len(tshape)].append(tshape)
-                for v in tvst:
-                    tvisit.add(v)
-    return bdict, tdict
+    return location, tdict
 
 def solution(game_board, table):
     answer = 0
-    bdict, tdict = find_location(game_board, table)
-    for k, value in bdict.items():
-        for loca in value:
-            if k not in tdict:
-                continue
-            for tidx, shape in enumerate(tdict[k]):
-                if loca == shape:
-                    answer += k
-                    del tdict[k][tidx]
+    location, tdict = find_location(game_board, table)
+    for loca in location:
+        k = len(loca)
+        if k not in tdict:continue
+        for tidx, shape in enumerate(tdict[k]):
+            if loca == shape:
+                answer += k
+                del tdict[k][tidx]
+                break
     return answer
 
-
-# print(solution([[0,0,0],[1,1,0],[1,1,1]],[[1,1,1],[1,0,0],[0,0,0]]))
-# print(solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],[[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]))
-print(solution([[0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0], [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0], [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1], [0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1], [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0], [1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0], [0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0]],[[1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1], [1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0], [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0], [1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1], [1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1], [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1], [1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1], [1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1], [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1]]))
-
 """
-{6: [[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)], [(0, 0), (0, 1), (0, 2), (1, 2), (1, 3), (1, 4)], [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (2, 2)], [(0, 0), (0, 1), (1, 1), (1, 2), (2, 0), (2, 1)], [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (2, 1)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 3)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1)], [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)], [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 2)], [(0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 1)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (2, 0)]]})
-{6: [[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)], [(0, 0), (0, 1), (1, 1), (1, 2), (2, 0), (2, 1)], [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)], [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 1)], [(0, 0), (0, 1), (0, 2), (1, 1), (2, 1), (3, 1)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (2, 0)], [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (2, 1)], [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 2)], [(0, 0), (0, 1), (0, 2), (1, 1), (2, 0), (2, 1)], [(0, 0), (0, 1), (1, 0), (2, 0), (2, 1), (3, 0)], [(0, 0), (0, 1), (1, 0), (1, 1), (2, 1), (3, 1)], [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)], [(0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 1)], [(0, 0), (1, 0), (2, 0), (2, 1), (3, 0), (4, 0)]]})
+game_board	                                    table	                                        result
+[[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],     [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],	    14
+ [1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]]	     [0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]
+[[0,0,0],[1,1,0],[1,1,1]]	                    [[1,1,1],[1,0,0],[0,0,0]]	                    0
 """
